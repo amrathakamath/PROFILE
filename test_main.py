3919e9dcc15eb79bbe7d81d574566c69cae49171
amrathakamath <amrathadkamath@gmail.com>
2025-04-01 16:42:24 +0530

    added test fiile

---
+++ test_main.py
@@ -0,0 +1,8 @@
+from fastapi.testclient import TestClient
+from app import app
+client=TestClient(app)
+def test_homepage():
+    response=client.get("/")
+    assert response.status_code==200
+    assert "<title>Amratha D Kamath - Portfolio</title>" in response.text
+ 
